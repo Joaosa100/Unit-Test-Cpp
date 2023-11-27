7868b465128b85704cb1c46b40cebbaa291c957e
Joaosa100 <joaovitorsa100@gmail.com>
2023-11-26 21:32:47 -0300

    update test

--- tests/teste-avaliador.cpp
+++ tests/teste-avaliador.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main(){
+    // preparando ambiente (arrange)
     Lance primeiroLance(Usuario("Fulano de tal"), 1000);
     Lance segundoLance(Usuario("Beltrano"), 2000);
     Leilao leilao("Fiat 147 0km");
@@ -9,9 +10,20 @@ int main(){
     leilao.recebeLance(segundoLance);
 
     Avaliador leiloeiro;
+
+    // executando codigo a ser testado (act)
     leiloeiro.avalia(leilao);
 
-    std::cout << leiloeiro.recuperaMaiorValor() << std::endl;
+    // verificando (assert)
+    float valorEsperado = 2000;
+
+    if (valorEsperado == leiloeiro.recuperaMaiorValor()){
+        std::cout << "TESTE OK" << std::endl;
+    } else{
+        std::cout << "TESTE FALHOU" << std::endl;
+    }
+    
+    //std::cout << leiloeiro.recuperaMaiorValor() << std::endl;
 
     return 0;
 }

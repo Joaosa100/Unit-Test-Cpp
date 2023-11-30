704436c244c6c56ef2f57798e0e51903195378d7
Joaosa100 <joaovitorsa100@gmail.com>
2023-11-30 06:49:12 -0300

    add sections

--- tests/teste-avaliador.cpp
+++ tests/teste-avaliador.cpp
@@ -23,48 +23,57 @@ Leilao emOrdemDecrescente(){
     return leilao;
 }
 
-TEST_CASE("Deve recuperar maior lance de leilao"){
+TEST_CASE("Avaliador"){
     // preparando ambiente (arrange)
-    Leilao leilao = GENERATE(emOrdemCrescente(), emOrdemDecrescente());
     Avaliador leiloeiro;
 
-    // executando codigo a ser testado (act)
-    leiloeiro.avalia(leilao);
+    SECTION("Leiloes ordenados"){
+        Leilao leilao = GENERATE(emOrdemCrescente(), emOrdemDecrescente());
 
-    // verificando (assert)
-    REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+        SECTION("Deve recuperar maior lance de leilao"){
+
+
+            // executando codigo a ser testado (act)
+            leiloeiro.avalia(leilao);
+
+            // verificando (assert)
+            REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+        }
+
+        SECTION("Deve recuperar menor lance de leilao"){
+            // preparando ambiente (arrange)
+            Leilao leilao = GENERATE(emOrdemCrescente(), emOrdemDecrescente());
+            Avaliador leiloeiro;
+
+            // executando codigo a ser testado (act)
+            leiloeiro.avalia(leilao);
+
+            // verificando (assert)
+            REQUIRE(1000 == leiloeiro.recuperaMenorValor());
+        }
+
+    }
+    
+    SECTION("Deve recuperar os 3 maiores lances"){
+        // preparando ambiente (arrange)
+        Lance primeiroLance(Usuario("Fulano de tal"), 1000);
+        Lance segundoLance(Usuario("Beltrano"), 2000);
+        Lance terceiroLance(Usuario("Ciclano"), 1500);
+        Lance quartoLance(Usuario("Avulso"), 2500);
+        Leilao leilao("Fiat 147 0km");
+        leilao.recebeLance(primeiroLance);
+        leilao.recebeLance(segundoLance);
+        leilao.recebeLance(terceiroLance);
+        leilao.recebeLance(quartoLance);
+
+        Avaliador leiloeiro;
+
+        // executando codigo a ser testado (act)
+        leiloeiro.avalia(leilao);
+
+        // verificando (assert)
+        std::vector<Lance> maiores3Lances = leiloeiro.recupera3MaioresLances();
+        REQUIRE (3 == maiores3Lances.size());
+    }
 }
 
-TEST_CASE("Deve recuperar menor lance de leilao"){
-    // preparando ambiente (arrange)
-    Leilao leilao = GENERATE(emOrdemCrescente(), emOrdemDecrescente());
-    Avaliador leiloeiro;
-
-    // executando codigo a ser testado (act)
-    leiloeiro.avalia(leilao);
-
-    // verificando (assert)
-    REQUIRE(1000 == leiloeiro.recuperaMenorValor());
-}
-
-TEST_CASE("Deve recuperar os 3 maiores lances"){
-    // preparando ambiente (arrange)
-    Lance primeiroLance(Usuario("Fulano de tal"), 1000);
-    Lance segundoLance(Usuario("Beltrano"), 2000);
-    Lance terceiroLance(Usuario("Ciclano"), 1500);
-    Lance quartoLance(Usuario("Avulso"), 2500);
-    Leilao leilao("Fiat 147 0km");
-    leilao.recebeLance(primeiroLance);
-    leilao.recebeLance(segundoLance);
-    leilao.recebeLance(terceiroLance);
-    leilao.recebeLance(quartoLance);
-
-    Avaliador leiloeiro;
-
-    // executando codigo a ser testado (act)
-    leiloeiro.avalia(leilao);
-
-    // verificando (assert)
-    std::vector<Lance> maiores3Lances = leiloeiro.recupera3MaioresLances();
-    REQUIRE (3 == maiores3Lances.size());
-}

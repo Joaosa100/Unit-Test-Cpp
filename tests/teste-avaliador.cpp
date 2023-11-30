c416470dbec8f55fa0d0635e08c76327ff53e39e
Joaosa100 <joaovitorsa100@gmail.com>
2023-11-30 06:09:05 -0300

    add vector test

--- tests/teste-avaliador.cpp
+++ tests/teste-avaliador.cpp
@@ -71,3 +71,25 @@ TEST_CASE("Deve recuperar menor lance de leilao em ordem crescente"){
     // verificando (assert)
     REQUIRE(1000 == leiloeiro.recuperaMenorValor());
 }
+
+TEST_CASE("Deve recuperar os 3 maiores lances"){
+    // preparando ambiente (arrange)
+    Lance primeiroLance(Usuario("Fulano de tal"), 1000);
+    Lance segundoLance(Usuario("Beltrano"), 2000);
+    Lance terceiroLance(Usuario("Ciclano"), 1500);
+    Lance quartoLance(Usuario("Avulso"), 2500);
+    Leilao leilao("Fiat 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+    leilao.recebeLance(terceiroLance);
+    leilao.recebeLance(quartoLance);
+
+    Avaliador leiloeiro;
+
+    // executando codigo a ser testado (act)
+    leiloeiro.avalia(leilao);
+
+    // verificando (assert)
+    std::vector<Lance> maiores3Lances = leiloeiro.recupera3MaioresLances();
+    REQUIRE (3 == maiores3Lances.size());
+}
